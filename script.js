e0f0b6b5d02b52c0da2982c0c4dbd93ab6b07c84
DaryaKlyukvina <lihtinadasha@gmail.com>
2026-02-28 22:48:00 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const toggleTheme = () => {
+  if (document.body.classList.contains('light')) {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+  }
+}
+
+// Установим начальную темму
+document.body.classList.add('dark');
+
+document.getElementById('theme-switch').addEventListener('click', toggleTheme);
